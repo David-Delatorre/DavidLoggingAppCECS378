7eceb913d88cd8524968ec9152447f3d5c7aa956
David <styxdlt@gmail.com>
2024-04-04 20:15:35 -0700

    Initial commit

---
+++ app/src/main/java/com/plcoding/backgroundlocationtracking/MainActivity.kt
@@ -0,0 +1,241 @@
+package com.plcoding.backgroundlocationtracking
+
+import android.Manifest
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.core.app.ActivityCompat
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import com.plcoding.backgroundlocationtracking.ui.theme.BackgroundLocationTrackingTheme
+
+class MainActivity : ComponentActivity() {
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION,
+            ),
+            0
+        )
+
+        setContent {
+            BackgroundLocationTrackingTheme {
+                Column(
+                    modifier = Modifier.fillMaxSize()
+                ) {
+                    Button(onClick = {
+                        Intent(applicationContext, LocationService::class.java).apply {
+                            action = LocationService.ACTION_START
+                            startService(this)
+                        }
+                    }) {
+                        Text(text = "Start")
+                    }
+                    Spacer(modifier = Modifier.height(16.dp))
+                    Button(onClick = {
+                        Intent(applicationContext, LocationService::class.java).apply {
+                            action = LocationService.ACTION_STOP
+                            startService(this)
+                        }
+                    }) {
+                        Text(text = "Stop")
+                    }
+                }
+                // on below line creating variable for freebase database
+                // and database reference.
+                val firebaseDatabase = FirebaseDatabase.getInstance();
+                val databaseReference = firebaseDatabase.getReference("LocationInfo");
+
+                // on below line we are calling method to display UI
+                firebaseUI(LocalContext.current, databaseReference)
+            }
+        }
+    }
+}
+
+@Composable
+fun firebaseUI(context: Context, databaseReference: DatabaseReference){
+    val latitude = remember{
+        mutableStateOf(TextFieldValue())
+    }
+    val longitude = remember{
+        mutableStateOf(TextFieldValue())
+    }
+
+
+    // on below line creating a column to display our retrieved list.
+    Column(
+        // adding modifier for our column
+        modifier = Modifier
+            .fillMaxHeight()
+            .fillMaxWidth()
+            .background(Color.White),
+        // on below line adding vertical and horizontal alignment for column.
+        verticalArrangement = Arrangement.Top, horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        Text(
+            text = "Add data to Firebase Realtime Database",
+            // in modifier we are specifying padding
+            // for our text from all sides.
+            modifier = Modifier.padding(10.dp),
+            // on below line we are specifying
+            // style for our text
+            style = TextStyle(
+                color = Color.Black, fontSize = 20.sp
+            ), fontWeight = FontWeight.Bold
+        )
+
+        // on below line we are creating
+        // a text field for our email.
+        TextField(
+            // on below line we are specifying
+            // value for our email text field.
+            value = latitude.value,
+
+            // on below line we are adding on
+            // value change for text field.
+            onValueChange = { latitude.value = it },
+
+            // on below line we are adding place holder
+            // as text as "Enter your email"
+            placeholder = { Text(text = "Enter latitude") },
+
+            // on below line we are adding modifier to it
+            // and adding padding to it and filling max width
+            modifier = Modifier
+                .padding(16.dp)
+                .fillMaxWidth(),
+
+            // on below line we are adding text style
+            // specifying color and font size to it.
+            textStyle = TextStyle(color = Color.Black, fontSize = 15.sp),
+
+            // on below line we are adding
+            // single line to it.
+            singleLine = true,
+        )
+
+        // on below line we are adding spacer
+        Spacer(modifier = Modifier.height(10.dp))
+
+        // on below line we are creating
+        // a text field for our email.
+        TextField(
+            // on below line we are specifying
+            // value for our email text field.
+            value = longitude.value,
+
+            // on below line we are adding on
+            // value change for text field.
+            onValueChange = { longitude.value = it },
+
+            // on below line we are adding place holder
+            // as text as "Enter your email"
+            placeholder = { Text(text = "Enter longitude") },
+
+            // on below line we are adding modifier to it
+            // and adding padding to it and filling max width
+            modifier = Modifier
+                .padding(16.dp)
+                .fillMaxWidth(),
+
+            // on below line we are adding text style
+            // specifying color and font size to it.
+            textStyle = TextStyle(color = Color.Black, fontSize = 15.sp),
+
+            // on below line we are adding
+            // single line to it.
+            singleLine = true,
+        )
+
+        // on below line we are adding spacer
+        Spacer(modifier = Modifier.height(10.dp))
+
+        // on below line creating button
+        Button(
+            onClick = {
+                // on below line we are adding data.
+                val locationObj = LocationObj(latitude.value.text, longitude.value.text)
+                val newDatabaseReference = databaseReference.push()
+                // we are use add value event listener method
+                // which is called with database reference.
+                databaseReference.addValueEventListener(object : ValueEventListener {
+                    override fun onDataChange(snapshot: DataSnapshot) {
+                        // inside the method of on Data change we are setting
+                        // our object class to our database reference.
+                        // data base reference will sends data to firebase.
+                        newDatabaseReference.setValue(locationObj)
+
+                        // after adding this data we
+                        // are showing toast message.
+                        Toast.makeText(
+                            context,
+                            "Data added to Firebase Database",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+
+                    override fun onCancelled(error: DatabaseError) {
+                        // if the data is not added or it is cancelled then
+                        // we are displaying a failure toast message.
+                        Toast.makeText(
+                            context,
+                            "Fail to add data $error",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                })
+            },
+            // on below line we are
+            // adding modifier to our button.
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+        ) {
+            // on below line we are adding text for our button
+            Text(text = "Add Location", modifier = Modifier.padding(8.dp))
+        }
+    }
+}
